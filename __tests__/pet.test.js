3349b09d882aba64ffdf726b03f9ab72544f4759
Paul Spencer <billsongames@gmail.com>
2022-10-20 19:59:45 +0100

    exercise complete

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,8 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-  it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+	it('returns an object', () => {
+		expect(new Pet('Fido')).toBeInstanceOf(Object);
 	});
 
 	it('pet has a name', () => {
